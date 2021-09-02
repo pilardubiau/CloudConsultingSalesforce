82796f189965974bdbf9debd729c2fac8e41bff3
Pilar Dubiau <pdubiau@gmail.com>
2021-09-02 10:44:59 -0300

    cambios LWC

--- CloudConsultingSalesforce/force-app/main/default/lwc/containerStaffAlocation/containerStaffAlocation.js
+++ CloudConsultingSalesforce/force-app/main/default/lwc/containerStaffAlocation/containerStaffAlocation.js
@@ -1,3 +1,9 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class ContainerStaffAlocation extends LightningElement {}
+export default class ContainerStaffAlocation extends LightningElement {
+
+ @api recordId
+
+ connectedCallback(){console.log(this.recordId)}
+
+}
--- CloudConsultingSalesforce/force-app/main/default/lwc/userDetail/userDetail.js
+++ CloudConsultingSalesforce/force-app/main/default/lwc/userDetail/userDetail.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
 
-export default class UserDetail extends LightningElement {}
+export default class UserDetail extends LightningElement {
+
+
+
+
+
+
+}
