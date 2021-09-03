2a245e94c2f0d46112ee1cc2bbd8abd1a86afcbc
rodrigocadiz47 <rodrigocadiz47@gmail.com>
2021-09-03 20:07:12 -0300

    usersList

--- CloudConsultingSalesforce/force-app/main/default/lwc/staffRoles/staffRoles.js
+++ CloudConsultingSalesforce/force-app/main/default/lwc/staffRoles/staffRoles.js
@@ -2,35 +2,17 @@ import { LightningElement, api, wire } from 'lwc';
 import getRequirementsForProject from '@salesforce/apex/ProjectClass.getRequirementsForProject'
 
 export default class LightningExampleAccordionMultiple extends LightningElement {
-    @api 
-    recordId;
-    
+    @api recordId;
+     
     @wire
     (getRequirementsForProject, { projectId: '$recordId' })
-    requireRoles
-        
-
-    activeSectionsMessage = '';
-
-    handleSectionToggle(event) {
-        const openSections = event.detail.openSections;
-
-        if (openSections.length === 0) {
-            this.activeSectionsMessage = 'All sections are closed';
-        } else {
-            this.activeSectionsMessage =
-                'Open sections: ' + openSections.join(', ');
-        }
-    }
+    requireRoles    
 
     handleSave(){
         let userList = this.template.querySelectorAll('c-user-list')
-        console.log(userList.length)
         userList.forEach((list)=>{
             const users = list.selectRowsData()
-            if(users.length){
-                addStaff(users)
-            }
+            console.log(JSON.parse(JSON.stringify(users)))
         })
     }
 }
--- CloudConsultingSalesforce/force-app/main/default/lwc/userList/userList.js
+++ CloudConsultingSalesforce/force-app/main/default/lwc/userList/userList.js
@@ -1,35 +1,48 @@
 import { LightningElement, api, wire } from 'lwc';
-import getAvailableUsersByRole from '@salesforce/apex/ProjectClass.getAvailableUsersByRole'
+import getAvailableUsers from '@salesforce/apex/ProjectClass.getAvailableUsers'
+
 
 
 export default class UserList extends LightningElement {
     @api recordId;
-    @api role;
+    @api lineItem;
+    usersList;
+    
+    @wire(getAvailableUsers, {projectId: '$recordId'})
+    users({data, error}){
+        if(data){
+            this.usersList = [];
+            for(let i=0; i<data.length; i++){
+                this.usersList.push({...data[i], Role: data[i].UserRole.Name})
+            }
+        }
+    }
 
-    @wire(getAvailableUsersByRole, {projectId: '$recordId',role:'$role'})
-    users
 
     columns = [
+        { label: 'Role', fieldName: 'Role'},
         { label: 'First Name', fieldName: 'FirstName'},
         { label: 'Last Name', fieldName: 'LastName'},
         { 
             label: 'Start Date', 
             fieldName: 'Start_Date__c', 
             editable: true, 
-            type: 'date', 
+            type: 'date-local', 
             typeAttributes: {year: "numeric",month: "2-digit",day: "2-digit"}
         },
         { 
             label: 'End Date', 
             fieldName: 'End_Date__c', 
-            editable: true, type: 'date', 
+            editable: true, 
+            type: 'date-local',
             typeAttributes: {year: "numeric",month: "2-digit",day: "2-digit"} 
         },
     ];
 
     @api selectRowsData(){
         const dataUserRows= this.template.querySelector('lightning-datatable').getSelectedRows()
-        return JSON.parse(JSON.stringify(dataUserRows))
+        const usersList = JSON.parse(JSON.stringify(dataUserRows))
+        console.log(usersList)
+        return { users: usersList, lineItem: this.lineItem }
     }
-
 }
