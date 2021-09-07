7003c0cd8c9cf45c00f26d01aea4154df9fae0f1
Pilar Dubiau <pdubiau@gmail.com>
2021-09-07 10:51:28 -0300

    version final

--- CloudConsultingSalesforce/force-app/main/default/lwc/staffRoles/staffRoles.js
+++ CloudConsultingSalesforce/force-app/main/default/lwc/staffRoles/staffRoles.js
@@ -5,27 +5,27 @@ import { getRecord } from 'lightning/uiRecordApi';
 import DISPLAY_DATE from '@salesforce/schema/Project__c.Date_Display__c'
 
 export default class LightningExampleAccordionMultiple extends LightningElement {
+
     @api recordId;
     requireRoles;
     projectDates;
-   
 
+    // Trae la fecha del proyecto 
     @wire(getRecord, { recordId: '$recordId', fields: DISPLAY_DATE })
     project({data, error}){
         if(data){
-            console.log('Data', data)
-            console.log('data date',data.fields.Date_Display__c.value)
             this.projectDates = data.fields.Date_Display__c.value;
         }
     }
 
+    //Trae los requerimientos del proyecto, mediante una llamada a un metodo de Apex
     @wire
     (getRequirementsForProject, { projectId: '$recordId' })
     roles(result){
         this.requireRoles = result
+    }   
     
-    }    
-
+    //Actualiza la informacion de los requerimientos
     handleAssign(){
         return refreshApex(this.requireRoles);
     }
